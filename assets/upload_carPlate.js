4304e6ed4fee4ab24793c6ad3758b4a75570205e
nivleM <vinmelx98@gmail.com>
2018-11-17 00:37:03 +0800

    3rd commit
    check

--- assets/upload_carPlate.js
+++ assets/upload_carPlate.js
@@ -10,54 +10,54 @@ function uploadPhoto() {
     formData.append("access_key", ACCESS_KEY);
     formData.append("secret_key", SECRET_KEY);
 
-$.ajax({
-    url:API_URL,
-    type:"POST",
-    data:formData,
-    processData:!1,
-    contentType:!1,
-    success:function(response){
-        document.getElementById("loader").style.display="none";
-        document.getElementById('response').innerHTML="";
-        responseJSON=response
-responseJSONstr=JSON.stringify(response,null,5);
-countLP=responseJSON.licensePlates.length
-strDisplay+="Number of license plate(s) detected: "+countLP+"\n";
-for(var plate=0;plate<countLP;plate++){
-    strDisplay+="\u2022 "+responseJSON.licensePlates[plate].licensePlateNumber+"\n"
+    $.ajax({
+        url: API_URL,
+        type: "POST",
+        data: formData,
+        processData: !1,
+        contentType: !1,
+        success: function (response) {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById('response').innerHTML = "";
+            responseJSON = response
+            responseJSONstr = JSON.stringify(response, null, 5);
+            countLP = responseJSON.licensePlates.length
+            strDisplay += "Number of license plate(s) detected: " + countLP + "\n";
+            for (var plate = 0; plate < countLP; plate++) {
+                strDisplay += "\u2022 " + responseJSON.licensePlates[plate].licensePlateNumber + "\n"
+            }
+            document.getElementById('response').innerHTML = strDisplay;
+            strDisplay = "";
+            // var clearBoxAfterDrawing=document.getElementById('boxDrawing');
+            // clear(clearBoxAfterDrawing);
+            // for(var numPlate=0;numPlate<countLP;numPlate++){
+            //     var newX=responseJSON.licensePlates[numPlate].boundingBox.x*(($('#uploadImg').width())/ori_width);
+            //     var newY=responseJSON.licensePlates[numPlate].boundingBox.y*(($('#uploadImg').height())/ori_height);
+            //     var newW=responseJSON.licensePlates[numPlate].boundingBox.w*(($('#uploadImg').width())/ori_width);
+            //     var newH=responseJSON.licensePlates[numPlate].boundingBox.h*(($('#uploadImg').height())/ori_height);
+            //     var canvas=document.getElementById("boxDrawing");
+            //     var ctx=canvas.getContext("2d");
+            //     ctx.beginPath();
+            //     ctx.strokeStyle="#00FA9A";
+            //     ctx.lineWidth="5";
+            //     ctx.rect(newX,newY,newW,newH);
+            //     ctx.stroke();
+            //     ctx.fillStyle="#00FA9A";
+            //     if(newX-2+120>$('#uploadImg').width()){
+            //         ctx.fillRect(newX-20-newW,newY-12,newW+5,10);
+            //         ctx.fillStyle="#000000";
+            //         ctx.font="13px Verdana";
+            //         ctx.fillText(responseJSON.licensePlates[numPlate].licensePlateNumber,newX-20-newW,newY-2)
+            //     }else{
+            //         ctx.fillRect(newX-2,newY-12,newW+5,10);
+            //         ctx.fillStyle="#000000";
+            //         ctx.font="13px Verdana";
+            //     ctx.fillText(responseJSON.licensePlates[numPlate].licensePlateNumber,newX-2,newY-2)}
+            // }
+        },
+        error: function (jqXHR, textStatus, errorMessage) {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById('response').innerHTML = errorMessage
+        }
+    })
 }
-document.getElementById('response').innerHTML=strDisplay;
-strDisplay="";
-// var clearBoxAfterDrawing=document.getElementById('boxDrawing');
-// clear(clearBoxAfterDrawing);
-// for(var numPlate=0;numPlate<countLP;numPlate++){
-//     var newX=responseJSON.licensePlates[numPlate].boundingBox.x*(($('#uploadImg').width())/ori_width);
-//     var newY=responseJSON.licensePlates[numPlate].boundingBox.y*(($('#uploadImg').height())/ori_height);
-//     var newW=responseJSON.licensePlates[numPlate].boundingBox.w*(($('#uploadImg').width())/ori_width);
-//     var newH=responseJSON.licensePlates[numPlate].boundingBox.h*(($('#uploadImg').height())/ori_height);
-//     var canvas=document.getElementById("boxDrawing");
-//     var ctx=canvas.getContext("2d");
-//     ctx.beginPath();
-//     ctx.strokeStyle="#00FA9A";
-//     ctx.lineWidth="5";
-//     ctx.rect(newX,newY,newW,newH);
-//     ctx.stroke();
-//     ctx.fillStyle="#00FA9A";
-//     if(newX-2+120>$('#uploadImg').width()){
-//         ctx.fillRect(newX-20-newW,newY-12,newW+5,10);
-//         ctx.fillStyle="#000000";
-//         ctx.font="13px Verdana";
-//         ctx.fillText(responseJSON.licensePlates[numPlate].licensePlateNumber,newX-20-newW,newY-2)
-//     }else{
-//         ctx.fillRect(newX-2,newY-12,newW+5,10);
-//         ctx.fillStyle="#000000";
-//         ctx.font="13px Verdana";
-    //     ctx.fillText(responseJSON.licensePlates[numPlate].licensePlateNumber,newX-2,newY-2)}
-    // }
-},
-    error:function(jqXHR,textStatus,errorMessage){
-        document.getElementById("loader").style.display="none";
-        document.getElementById('response').innerHTML=errorMessage}}
-        )}
-
-
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 const port = 3000;
+let options = {};
 
-//express.static(root, [options]);
+// express.static(root, [options]);
 app.use(express.static('assets'));
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    
 });
- 
 
 app.listen(port, () => console.log(`Server side working`));
