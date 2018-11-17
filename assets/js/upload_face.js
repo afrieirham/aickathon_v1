e02af6fe13e909341aff70c02592437043fe6494
nivleM <vinmelx98@gmail.com>
2018-11-17 10:48:29 +0800

    working + bootstrap

--- assets/js/upload_face.js
+++ assets/js/upload_face.js
@@ -68,7 +68,7 @@ function uploadPhoto() {
     var API_URL = 'https://face.recoqnitics.com/analyze'
     var ACCESS_KEY = 'cc7d67f2adb06edcb419';
     var SECRET_KEY = 'e10f180f2fdf99ec9eae2c92c4e3bc8c9373198f';
-    var formData = new FormData(document.getElementById("faceInput"))
+    var formData = new FormData(document.forms.namedItem('fileinfo'))
     formData.append("access_key", ACCESS_KEY);
     formData.append("secret_key", SECRET_KEY);
 
